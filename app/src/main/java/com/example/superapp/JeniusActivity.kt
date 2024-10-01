1f204d9f8b02687a5036a65d7e143f2dfe0ee067
MICHAEL.PANGESTU <michael.pangestu@btpn.com>
2024-10-01 11:59:34 +0700

    upload to github

---
+++ app/src/main/java/com/example/superapp/JeniusActivity.kt
@@ -0,0 +1,28 @@
+package com.example.superapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class JeniusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_jenius)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val goToHomeButton: Button = findViewById(R.id.goToHomeButton)
+        goToHomeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
